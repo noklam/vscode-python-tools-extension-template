20b02bc6b8afed4ac1b8a145b8ca195cf483904d
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-05-01 16:27:07 +0100

    add some simple tests

--- src/test/python_tests/test_server.py
+++ src/test/python_tests/test_server.py
@@ -1,138 +1,63 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-"""
-Test for linting over LSP.
-"""
+import sys
 
-from threading import Event
+import pytest
+import pytest_lsp
+from lsprotocol.types import (
+    CompletionParams,
+    InitializeParams,
+    Position,
+    TextDocumentIdentifier,
+)
+from pytest_lsp import (
+    ClientServerConfig,
+    LanguageClient,
+    client_capabilities,
+)
 
-from hamcrest import assert_that, is_
 
-from .lsp_test_client import constants, defaults, session, utils
-
-TEST_FILE_PATH = constants.TEST_DATA / "sample1" / "sample.py"
-TEST_FILE_URI = utils.as_uri(str(TEST_FILE_PATH))
-SERVER_INFO = utils.get_server_info_defaults()
-TIMEOUT = 10  # 10 seconds
-
-
-def test_linting_example():
-    """Test to linting on file open."""
-    contents = TEST_FILE_PATH.read_text()
-
-    actual = []
-    with session.LspSession() as ls_session:
-        ls_session.initialize(defaults.VSCODE_DEFAULT_INITIALIZE)
-
-        done = Event()
-
-        def _handler(params):
-            nonlocal actual
-            actual = params
-            done.set()
-
-        ls_session.set_notification_callback(session.PUBLISH_DIAGNOSTICS, _handler)
-
-        ls_session.notify_did_open(
-            {
-                "textDocument": {
-                    "uri": TEST_FILE_URI,
-                    "languageId": "python",
-                    "version": 1,
-                    "text": contents,
-                }
-            }
+@pytest_lsp.fixture(
+    config=ClientServerConfig(
+        server_command=[sys.executable, "/Users/Nok_Lam_Chan/dev/vscode-python-tools-extension-template/bundled/tool/lsp_server.py"],
+    ),
+)
+async def client(lsp_client: LanguageClient):
+    # Setup
+    response = await lsp_client.initialize_session(
+        InitializeParams(
+            capabilities=client_capabilities("visual-studio-code"),
+            root_uri="file:///Users/Nok_Lam_Chan/dev/pygls/examples/servers/old_kedro_project",
         )
+    )
 
-        # wait for some time to receive all notifications
-        done.wait(TIMEOUT)
+    yield
 
-        # TODO: Add your linter specific diagnostic result here
-        expected = {
-            "uri": TEST_FILE_URI,
-            "diagnostics": [
-                {
-                    # "range": {
-                    #     "start": {"line": 0, "character": 0},
-                    #     "end": {"line": 0, "character": 0},
-                    # },
-                    # "message": "Missing module docstring",
-                    # "severity": 3,
-                    # "code": "C0114:missing-module-docstring",
-                    "source": SERVER_INFO["name"],
-                },
-                {
-                    # "range": {
-                    #     "start": {"line": 2, "character": 6},
-                    #     "end": {
-                    #         "line": 2,
-                    #         "character": 7,
-                    #     },
-                    # },
-                    # "message": "Undefined variable 'x'",
-                    # "severity": 1,
-                    # "code": "E0602:undefined-variable",
-                    "source": SERVER_INFO["name"],
-                },
-                {
-                    # "range": {
-                    #     "start": {"line": 0, "character": 0},
-                    #     "end": {
-                    #         "line": 0,
-                    #         "character": 10,
-                    #     },
-                    # },
-                    # "message": "Unused import sys",
-                    # "severity": 2,
-                    # "code": "W0611:unused-import",
-                    "source": SERVER_INFO["name"],
-                },
-            ],
-        }
+    # Teardown
+    await lsp_client.shutdown_session()
 
-    assert_that(actual, is_(expected))
+@pytest.mark.asyncio
+async def test_completion(client: LanguageClient):
+    result = await client.text_document_completion_async(
+        params=CompletionParams(
+            position=Position(line=5, character=23),
+            text_document=TextDocumentIdentifier(
+                uri="file:///path/to/test/project/root/test_file.rst"
+            ),
+        )
+    )
 
+    assert len(result.items) > 0
 
-def test_formatting_example():
-    """Test formatting a python file."""
-    FORMATTED_TEST_FILE_PATH = constants.TEST_DATA / "sample1" / "sample.py"
-    UNFORMATTED_TEST_FILE_PATH = constants.TEST_DATA / "sample1" / "sample.unformatted"
+@pytest.mark.xfail(reason="This should fail but currently autocompletion is trigger for every file")
+@pytest.mark.asyncio
+async def test_completion_fail(client: LanguageClient):
+    result = await client.text_document_completion_async(
+        params=CompletionParams(
+            position=Position(line=5, character=23),
+            text_document=TextDocumentIdentifier(
+                uri="file:///path/to/test/project/root/test_file.rst"
+            ),
+        )
+    )
 
-    contents = UNFORMATTED_TEST_FILE_PATH.read_text()
-    lines = contents.splitlines(keepends=False)
+    assert len(result.items) < 1
 
-    actual = []
-    with utils.PythonFile(contents, UNFORMATTED_TEST_FILE_PATH.parent) as pf:
-        uri = utils.as_uri(str(pf.fullpath))
-
-        with session.LspSession() as ls_session:
-            ls_session.initialize()
-            ls_session.notify_did_open(
-                {
-                    "textDocument": {
-                        "uri": uri,
-                        "languageId": "python",
-                        "version": 1,
-                        "text": contents,
-                    }
-                }
-            )
-            actual = ls_session.text_document_formatting(
-                {
-                    "textDocument": {"uri": uri},
-                    # `options` is not used by black
-                    "options": {"tabSize": 4, "insertSpaces": True},
-                }
-            )
-
-    expected = [
-        {
-            "range": {
-                "start": {"line": 0, "character": 0},
-                "end": {"line": len(lines), "character": 0},
-            },
-            "newText": FORMATTED_TEST_FILE_PATH.read_text(),
-        }
-    ]
-
-    assert_that(actual, is_(expected))
